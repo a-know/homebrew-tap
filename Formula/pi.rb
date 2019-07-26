6c44213e5bd2aec8b5c492a308130bccec75dbf9
a-know <a-know@hatena.ne.jp>
2019-07-26 15:17:47 +0900

    Update pi v1.1.0

--- Formula/pi.rb
+++ Formula/pi.rb
@@ -1,8 +1,8 @@
 class Pi < Formula
-    version '1.0.3'
+    version '1.1.0'
     homepage 'https://github.com/a-know/pi'
-    url "https://github.com/a-know/pi/releases/download/v1.0.3/pi_v1.0.3_darwin_amd64.zip"
-    sha256 '33e8b3b982808cc370fd3023f68bbec9981d1b903249dbdc0d307f6cddb17c5d'
+    url "https://github.com/a-know/pi/releases/download/v1.1.0/pi_v1.1.0_darwin_amd64.zip"
+    sha256 '29dc34fddf6d65387899363df9575c1eed9110359d950d6512bb100008defe30'
     head 'https://github.com/a-know/pi.git'
   
     def install
